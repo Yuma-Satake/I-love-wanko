0dbbce79adb7da93ba389179bd95fee7f320a28c
Yuma Satake <satous.y@icloud.com>
2023-08-15 20:34:56 +0900

    fix: レイアウト修正

--- src/Description.js
+++ src/Description.js
@@ -1,25 +1,35 @@
 // DO NOT DELETE
 import * as React from 'react'
-import { useState } from 'react';
-import { DogImage } from './DogImage';
+import { useState } from 'react'
+import { DogImage } from './DogImage'
+import { Button, Container } from '@mui/material'
 
 export const Description = () => {
-  const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/shiba/shiba-16.jpg');
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/shiba/shiba-16.jpg',
+  )
 
   const handleUpdate = () => {
     fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
       .then(data => setDogUrl(data.message))
-      .catch(error => console.log(error));
-  };
+      .catch(error => console.log(error))
+  }
 
   return (
     <div id="flex">
       <h2>愛しいわんこを眺めるためのサイトです</h2>
-      <DogImage url={dogUrl} />
-      <button type="button" onClick={handleUpdate} id="updateButton">
-          更新
-      </button>
+      <Container>
+        <DogImage url={dogUrl} />
+      </Container>
+      <Button
+        variant="contained"
+        type="button"
+        onClick={handleUpdate}
+        id="updateButton"
+      >
+        更新
+      </Button>
     </div>
-  );
-};
+  )
+}
